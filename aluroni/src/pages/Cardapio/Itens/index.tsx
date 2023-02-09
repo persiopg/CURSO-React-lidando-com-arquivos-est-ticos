740764fea36832613db0676bca666261149eac41
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2023-02-09 16:57:02 -0300

    correç~es bugs no eslint

--- aluroni/src/pages/Cardapio/Itens/index.tsx
+++ aluroni/src/pages/Cardapio/Itens/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Item from './Item';
 import cardapio from './itens.json';
 import styles from './Itens.module.scss';
@@ -31,14 +31,14 @@ export default function Itens(props: Props) {
 
   function ordenar(novaLista: typeof cardapio) {
     switch (ordenador) {
-    case 'porcao':
-      return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
-    case 'qtd_pessoas':
-      return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
-    case 'preco':
-      return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
-    default:
-      return novaLista;
+      case 'porcao':
+        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      case 'qtd_pessoas':
+        return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+      case 'preco':
+        return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+      default:
+        return novaLista;
     }
   }
 
