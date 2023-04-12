050c5c194575fbc761371506a1b58b5774537d3f
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2023-04-12 14:42:42 -0300

    Reaproveitando com react-router

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
+
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
@@ -12,9 +12,6 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState('');
   return (
     <main>
-      <nav className={styles.menu}>
-        <Logo />
-      </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
