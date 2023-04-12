7db0ddbb594e11e66716ff00bebabaf76d575743
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2023-04-12 14:12:01 -0300

    .ok

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
+import Router from './routers.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>    
-    <Cardapio/>  
+  <React.StrictMode>
+    <Router />
   </React.StrictMode>
 );
